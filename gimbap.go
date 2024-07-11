425a0bc5993ca46521478ef7afa6ae3a8840b6dd
Jonghyun Seong <jhseong7@gmail.com>
2024-07-11 16:33:11 +0900

    Feat: Add type alias for public entry point

--- gimbap.go
+++ gimbap.go
@@ -2,6 +2,34 @@ package gimbap
 
 import (
 	"github.com/jhseong7/gimbap/app"
+	"github.com/jhseong7/gimbap/controller"
+	"github.com/jhseong7/gimbap/engine"
+	"github.com/jhseong7/gimbap/module"
+	"github.com/jhseong7/gimbap/provider"
+)
+
+// type aliases for public apis
+type (
+	// App related
+	AppOption      = app.AppOption
+	GimbapApp      = app.GimbapApp
+	RuntimeOptions = app.RuntimeOptions
+
+	// Module related
+	ModuleOption = module.ModuleOption
+	Module       = module.Module
+
+	// Provider related
+	ProviderDefinition = provider.ProviderDefinition
+
+	// Controller related
+	IController          = controller.IController
+	ControllerDefinition = controller.ControllerDefinition
+	RouteSpec            = controller.RouteSpec
+
+	// Engine related
+	IHttpEngine      = engine.IHttpEngine
+	HttpEngineOption = engine.HttpEngineOption
 )
 
 // Public entry point to create a Gimbap application.
