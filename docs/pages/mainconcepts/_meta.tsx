96a41a8f7d2ce483f628654d17a48780055ab66f
jhseong7 <jhseong7@gmail.com>
2024-10-07 03:28:47 +0900

    Docs: Update docs (#45)

---
+++ docs/pages/mainconcepts/_meta.tsx
@@ -0,0 +1,9 @@
+export default {
+  introduction: "Introduction",
+  app: "App Container",
+  depsmanager: "Dependency Manager",
+  serverengine: "Server Engine",
+  microservices: "Microservices",
+  moduleprovider: "Modules and Providers",
+  controller: "Controller",
+};
